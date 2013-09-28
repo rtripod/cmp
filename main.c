9f00f726d73bb7454e3f61ece799c8950face588
Ryan <rtripod@gmail.com>
2013-09-28 13:22:44 +0800

    Implemented FSM behaviour

--- main.c
+++ main.c
@@ -4,13 +4,25 @@
 #define LINE_SENSOR_MAX 300
 #define FORCE_SENSOR_MAX 900
 
+#define FINISHED_OPERATION 1
+#define CONTINUE_OPERATION 0
+
+typedef enum {STATE_START, STATE_POST_FERRIS, STATE_DUCKS, STATE_STRENGTH, STATE_FINISH} STATE;
+
+void state_machine(STATE *, int);
+void state_start(int *);
+void state_post_ferris(int *);
+void state_ducks(int *);
+void state_strength(int *);
+void state_finish(int *);
+
 volatile unsigned int ADCResult;
 
-void SetupADC(void)
+void SetupADC(unsigned int in_bit)
 {
 	// Configure ADC
-	P1SEL1 |= BIT4;  
-	P1SEL0 |= BIT4; 
+	P1SEL1 |= in_bit;  
+	P1SEL0 |= in_bit; 
 
 	// Allow for settling delay 
 	__delay_cycles(50000);
@@ -21,7 +33,26 @@ void SetupADC(void)
 	// ADCCLK = MODOSC = 5MHz
 	ADC10CTL1 = ADC10SHS_0 + ADC10SHP + ADC10SSEL_0; 
 	ADC10CTL2 = ADC10RES;                    // 10-bit conversion results
-	ADC10MCTL0 = ADC10INCH_4;                // A4 ADC input select; Vref=AVCC
+	
+	switch (in_bit)
+	{
+		case BIT0:
+			ADC10MCTL0 = ADC10INCH_0;
+			break;
+		case BIT1:
+			ADC10MCTL0 = ADC10INCH_1;
+			break;
+		case BIT2:
+			ADC10MCTL0 = ADC10INCH_2;
+			break;
+		case BIT3:
+			ADC10MCTL0 = ADC10INCH_3;
+			break;
+		case BIT4:
+			ADC10MCTL0 = ADC10INCH_4;
+			break;
+	}	
+	
 	ADC10IE = ADC10IE0;                      // Enable ADC conv complete interrupt
 
 	// Setup Thresholds for relative difference in Thermistor measurements
@@ -49,13 +80,6 @@ unsigned int CalibrateADC(void)
 	return Value;
 }
 
-/**********************************************************************//**
-* @brief  Take ADC Measurement
-* 
-* @param  none 
-*  
-* @return none
-*************************************************************************/
 void TakeADCMeas(void)
 {  
 	while (ADC10CTL1 & BUSY); 
@@ -64,14 +88,7 @@ void TakeADCMeas(void)
 	__no_operation();                       // For debug only
 }
 
-/**********************************************************************//**
-* @brief  Initializes Accelerometer
-* 
-* @param  none 
-*  
-* @return none
-*************************************************************************/
-void main(void)
+int main(void)
 {
 	WDTCTL = WDTPW + WDTHOLD;
 	// Init SMCLK = MCLk = ACLK = 1MHz
@@ -79,31 +96,157 @@ void main(void)
 	CSCTL1 |= DCOFSEL0 + DCOFSEL1;          // Set max. DCO setting = 8MHz
 	CSCTL2 = SELA_3 + SELS_3 + SELM_3;      // set ACLK = MCLK = DCO
 	CSCTL3 = DIVA_3 + DIVS_3 + DIVM_3;      // set all dividers to 1MHz
-
+	
 	// P1.4 is used as input from NTC voltage divider
 	// Set it to output low
 	P1OUT &= ~BIT4;      
 	P1DIR |= BIT4; 
-	PJDIR |= (BIT0+BIT1);
-
-	SetupADC();
-	//unsigned int compare = CalibrateADC();
+	
+	PJDIR |= (BIT0+BIT1+BIT2+BIT3);
+	P3DIR |= (BIT4+BIT5+BIT6+BIT7);
+	
+	PJOUT &= ~(BIT0+BIT1+BIT2+BIT3);
+	P3OUT &= ~(BIT4+BIT5+BIT6+BIT7);
+	
+	STATE state = STATE_START;
+	int operation;
 	while(1)
-	{      
-	TakeADCMeas();
-     
-     //printf("%d \n", ADCResult);
+	{
+		PJOUT &= ~(BIT0+BIT1+BIT2+BIT3);
+		P3OUT &= ~(BIT4+BIT5+BIT6+BIT7);
+		switch(state)
+		{
+			case STATE_START:
+				PJOUT |= BIT0;
+				__delay_cycles(50000);
+				state_start(&operation);
+				break;
+			case STATE_POST_FERRIS:
+				PJOUT |= BIT1;
+				__delay_cycles(50000);
+				state_ducks(&operation);
+				break;
+			case STATE_DUCKS:
+				PJOUT |= BIT2;
+				__delay_cycles(50000);
+				state_ducks(&operation);
+				break;
+			case STATE_STRENGTH:
+				PJOUT |= BIT3;
+				__delay_cycles(50000);
+				state_strength(&operation);
+				break;
+			case STATE_FINISH:
+				P3OUT |= BIT4;
+				__delay_cycles(50000);
+				state_finish(&operation);
+				break;
+		}
+		__delay_cycles(50000);
+		state_machine(&state, operation);
+	}
+}
 
-/*	if( ADCResult < LINE_SENSOR_MAX )
-		PJOUT &= ~BIT0;
+void state_machine(STATE *state, int operation)
+{
+	switch(*state)
+	{
+		case STATE_START:
+			switch(operation)
+			{
+				case FINISHED_OPERATION:
+					*state = STATE_DUCKS;
+					break;
+			}
+			break;
+		case STATE_POST_FERRIS:
+			switch(operation)
+			{
+				case FINISHED_OPERATION:
+					*state = STATE_DUCKS;
+					break;
+			}
+			break;
+		case STATE_DUCKS:
+			switch(operation)
+			{
+				case FINISHED_OPERATION:
+					*state = STATE_STRENGTH;
+					break;
+			}
+			break;
+		case STATE_STRENGTH:
+			switch(operation)
+			{
+				case FINISHED_OPERATION:
+					*state = STATE_FINISH;
+					break;
+			}
+			break;
+		case STATE_FINISH:
+			switch(operation)
+			{
+				case FINISHED_OPERATION:
+					*state = STATE_START;
+					break;
+			}
+			break;
+	}
+}
+
+void state_start(int *operation)
+{
+	__delay_cycles(50000);
+	SetupADC(BIT0);
+	TakeADCMeas();
+	if(ADCResult < FORCE_SENSOR_MAX)
+		*operation = FINISHED_OPERATION;
 	else
-		PJOUT |= BIT0;*/
-     
-	if( ADCResult < FORCE_SENSOR_MAX )
-		PJOUT &= ~BIT1;
+		*operation = CONTINUE_OPERATION;
+}
+
+void state_post_ferris(int *operation)
+{
+	__delay_cycles(50000);
+	SetupADC(BIT1);
+	TakeADCMeas();
+	if(ADCResult < FORCE_SENSOR_MAX)
+		*operation = FINISHED_OPERATION;
 	else
-		PJOUT |= BIT1;  
-   }
+		*operation = CONTINUE_OPERATION;
+}
+
+void state_ducks(int *operation)
+{
+	__delay_cycles(50000);
+	SetupADC(BIT2);
+	TakeADCMeas();
+	if(ADCResult < LINE_SENSOR_MAX)
+		*operation = FINISHED_OPERATION;
+	else
+		*operation = CONTINUE_OPERATION;
+}
+
+void state_strength(int *operation)
+{
+	__delay_cycles(50000);
+	SetupADC(BIT3);
+	TakeADCMeas();
+	if(ADCResult < LINE_SENSOR_MAX)
+		*operation = FINISHED_OPERATION;
+	else
+		*operation = CONTINUE_OPERATION;
+}
+
+void state_finish(int *operation)
+{
+	__delay_cycles(50000);
+	SetupADC(BIT4);
+	TakeADCMeas();
+	if(ADCResult < FORCE_SENSOR_MAX)
+		*operation = FINISHED_OPERATION;
+	else
+		*operation = CONTINUE_OPERATION;
 }
 
 #pragma vector=ADC10_VECTOR

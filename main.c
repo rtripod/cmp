350dc33d8ae5db8c2950805a6f826952b99939e3
Ryan <rtripod@gmail.com>
2013-10-06 14:19:54 +0800

    Moved transitions to state_machine function
    Changed ints to unsigned chars.
    Fixed delayMillis function and replaced __delay_cycle calls.

--- main.c
+++ main.c
@@ -4,25 +4,41 @@
 #define LINE_SENSOR_MAX 300
 #define FORCE_SENSOR_MAX 900
 
-#define FINISHED_OPERATION 1
-#define CONTINUE_OPERATION 0
+#define FINISHED_OPERATION 0
+#define CONTINUE_OPERATION 1
 
-typedef enum {STATE_START, STATE_POST_FERRIS, STATE_DUCKS, STATE_STRENGTH, STATE_FINISH} STATE;
+typedef enum {STATE_IDLE, STATE_FERRIS, STATE_DUCKS, STATE_STRENGTH, STATE_FINISH} STATE;
 
-void state_machine(STATE *, int);
-void state_start(int *);
-void state_post_ferris(int *);
-void state_ducks(int *);
-void state_strength(int *);
-void state_finish(int *);
+void state_machine(STATE *, unsigned char);
+void state_idle(unsigned char *);
+void state_ferris(unsigned char *);
+void state_ducks(unsigned char *);
+void state_strength(unsigned char *);
+void state_finish(unsigned char *);
 
 volatile unsigned int ADCResult;
 
-void delay_ms(unsigned int delay)
+void delayMillis(unsigned int delay)
 {
 	while (delay--)
 	{
-		__delay_cycles(24000);//PUT_CPU_CLOCK_SPEED_IN_HZ_DIVIDED_BY_1000_HERE
+		__delay_cycles(1000);	// CPU clock speed divided by 1000
+	}
+}
+
+void updateLEDs(unsigned int in_bit)
+{
+	PJOUT &= ~(BIT0+BIT1+BIT2+BIT3);
+	P3OUT &= ~(BIT4+BIT5+BIT6+BIT7);
+	switch (in_bit)
+	{
+		case BIT0: case BIT1: case BIT2: case BIT3: 
+			PJOUT |= in_bit;
+			break;
+		case BIT4: case BIT5: case BIT6: case BIT7: 
+			P3OUT |= in_bit;
+			break;
+		default: break;
 	}
 }
 
@@ -33,14 +49,13 @@ void SetupADC(unsigned int in_bit)
 	P1SEL0 |= in_bit; 
 
 	// Allow for settling delay 
-	__delay_cycles(50000);
+	delayMillis(50);
 
 	// Configure ADC
 	ADC10CTL0 &= ~ADC10ENC; 
-	ADC10CTL0 = ADC10SHT_7 + ADC10ON;        // ADC10ON, S&H=192 ADC clks
-	// ADCCLK = MODOSC = 5MHz
+	ADC10CTL0 = ADC10SHT_7 + ADC10ON;		// ADC10ON, S&H=192 ADC clks
 	ADC10CTL1 = ADC10SHS_0 + ADC10SHP + ADC10SSEL_0; 
-	ADC10CTL2 = ADC10RES;                    // 10-bit conversion results
+	ADC10CTL2 = ADC10RES;					// 10-bit conversion results
 	
 	switch (in_bit)
 	{
@@ -61,118 +76,102 @@ void SetupADC(unsigned int in_bit)
 			break;
 	}	
 	
-	ADC10IE = ADC10IE0;                      // Enable ADC conv complete interrupt
-
-	// Setup Thresholds for relative difference in Thermistor measurements
-}
-
-unsigned int CalibrateADC(void)
-{
-	unsigned char CalibCounter =0;
-	unsigned int Value = 0;
-
-	// Disable interrupts & user input during calibration
-
-	while(CalibCounter <50)
-	{
-		P3OUT ^= BIT4;
-		CalibCounter++;
-		while (ADC10CTL1 & BUSY); 
-		ADC10CTL0 |= ADC10ENC | ADC10SC ;       // Start conversion 
-		__bis_SR_register(CPUOFF + GIE);        // LPM0, ADC10_ISR will force exit
-		__no_operation(); 
-		Value += ADCResult;
-	}
-	Value = Value/50;
-	// Reenable switches after calibration
-	return Value;
+	ADC10IE = ADC10IE0;						// Enable ADC conv complete interrupt
 }
 
 void TakeADCMeas(void)
 {  
 	while (ADC10CTL1 & BUSY); 
-	ADC10CTL0 |= ADC10ENC | ADC10SC ;       // Start conversion 
-	__bis_SR_register(CPUOFF + GIE);        // LPM0, ADC10_ISR will force exit
-	__no_operation();                       // For debug only
+	ADC10CTL0 |= ADC10ENC | ADC10SC ;		// Start conversion 
+	__bis_SR_register(CPUOFF + GIE);		// LPM0, ADC10_ISR will force exit
+	__no_operation();						// For debug only
 }
 
 int main(void)
 {
 	WDTCTL = WDTPW + WDTHOLD;
 	// Init SMCLK = MCLk = ACLK = 1MHz
-	CSCTL0_H = 0xA5;
-	CSCTL1 |= DCOFSEL0 + DCOFSEL1;          // Set max. DCO setting = 8MHz
-	CSCTL2 = SELA_3 + SELS_3 + SELM_3;      // set ACLK = MCLK = DCO
-	CSCTL3 = DIVA_3 + DIVS_3 + DIVM_3;      // set all dividers to 1MHz
+	CSCTL0_H = 0xA5;						// Password
+	CSCTL1 |= DCOFSEL0 + DCOFSEL1;			// Set max. DCO setting = 8MHz
+	CSCTL2 = SELA_3 + SELS_3 + SELM_3;		// set ACLK = MCLK = DCO/8
+	CSCTL3 = DIVA_3 + DIVS_3 + DIVM_3;		// set all dividers to 1MHz
 	
 	// P1.4 is used as input from NTC voltage divider
 	// Set it to output low
 	P1OUT &= ~BIT4;      
 	P1DIR |= BIT4; 
 	
+	// Setup onboard LEDs
 	PJDIR |= (BIT0+BIT1+BIT2+BIT3);
 	P3DIR |= (BIT4+BIT5+BIT6+BIT7);
-	
 	PJOUT &= ~(BIT0+BIT1+BIT2+BIT3);
 	P3OUT &= ~(BIT4+BIT5+BIT6+BIT7);
 	
-	STATE state = STATE_START;
-	int operation;
+	// Setup geared motor
+	P2DIR |= BIT0;
+	P2OUT &= ~BIT0;
+	
+	// Inital state
+	STATE state = STATE_IDLE;
+	PJOUT |= BIT0;
+	SetupADC(BIT0);
+	
+	unsigned char operation;
 	while(1)
 	{
-		PJOUT &= ~(BIT0+BIT1+BIT2+BIT3);
-		P3OUT &= ~(BIT4+BIT5+BIT6+BIT7);
 		switch(state)
 		{
-			case STATE_START:
-				PJOUT |= BIT0;
-				__delay_cycles(50000);
-				state_start(&operation);
+			case STATE_IDLE:
+				delayMillis(50);
+				state_idle(&operation);
 				break;
-			case STATE_POST_FERRIS:
-				PJOUT |= BIT1;
-				__delay_cycles(50000);
-				state_post_ferris(&operation);
+			case STATE_FERRIS:
+				delayMillis(50);
+				state_ferris(&operation);
 				break;
 			case STATE_DUCKS:
-				PJOUT |= BIT2;
-				__delay_cycles(50000);
+				delayMillis(50);
 				state_ducks(&operation);
 				break;
 			case STATE_STRENGTH:
-				PJOUT |= BIT3;
-				__delay_cycles(50000);
+				delayMillis(50);
 				state_strength(&operation);
 				break;
 			case STATE_FINISH:
-				P3OUT |= BIT4;
-				__delay_cycles(50000);
+				delayMillis(50);
 				state_finish(&operation);
 				break;
 		}
-		__delay_cycles(50000);
+		delayMillis(50);
 		state_machine(&state, operation);
 	}
 }
 
-void state_machine(STATE *state, int operation)
+void state_machine(STATE *state, unsigned char operation)
 {
 	switch(*state)
 	{
-		case STATE_START:
+		case STATE_IDLE:
 			switch(operation)
 			{
 				case FINISHED_OPERATION:
-					*state = STATE_POST_FERRIS;
+					*state = STATE_FERRIS;
+					updateLEDs(BIT1);
+					SetupADC(BIT1);
+					P2OUT |= BIT0;		// Turn on geared motor
 					break;
+				default: break;
 			}
 			break;
-		case STATE_POST_FERRIS:
+		case STATE_FERRIS:
 			switch(operation)
 			{
 				case FINISHED_OPERATION:
 					*state = STATE_DUCKS;
+					updateLEDs(BIT2);
+					SetupADC(BIT2);
 					break;
+				default: break;
 			}
 			break;
 		case STATE_DUCKS:
@@ -180,7 +179,10 @@ void state_machine(STATE *state, int operation)
 			{
 				case FINISHED_OPERATION:
 					*state = STATE_STRENGTH;
+					updateLEDs(BIT3);
+					SetupADC(BIT3);
 					break;
+				default: break;
 			}
 			break;
 		case STATE_STRENGTH:
@@ -188,24 +190,31 @@ void state_machine(STATE *state, int operation)
 			{
 				case FINISHED_OPERATION:
 					*state = STATE_FINISH;
+					updateLEDs(BIT4);
+					SetupADC(BIT4);
 					break;
+				default: break;
 			}
 			break;
 		case STATE_FINISH:
 			switch(operation)
 			{
 				case FINISHED_OPERATION:
-					*state = STATE_START;
+					*state = STATE_IDLE;
+					updateLEDs(BIT0);
+					SetupADC(BIT0);
+					P2OUT &= ~BIT0;		// Turn off geared motor
 					break;
+				default: break;
 			}
 			break;
 	}
 }
 
-void state_start(int *operation)
+void state_idle(unsigned char *operation)
 {
-	__delay_cycles(50000);
-	SetupADC(BIT0);
+	
+	delayMillis(50);
 	TakeADCMeas();
 	if(ADCResult < FORCE_SENSOR_MAX)
 		*operation = FINISHED_OPERATION;
@@ -213,9 +222,9 @@ void state_start(int *operation)
 		*operation = CONTINUE_OPERATION;
 }
 
-void state_post_ferris(int *operation)
+void state_ferris(unsigned char *operation)
 {
-	__delay_cycles(50000);
+	delayMillis(50);
 	SetupADC(BIT1);
 	TakeADCMeas();
 	if(ADCResult < FORCE_SENSOR_MAX)
@@ -224,31 +233,31 @@ void state_post_ferris(int *operation)
 		*operation = CONTINUE_OPERATION;
 }
 
-void state_ducks(int *operation)
+void state_ducks(unsigned char *operation)
 {
-	__delay_cycles(50000);
+	delayMillis(50);
 	SetupADC(BIT2);
 	TakeADCMeas();
-	if(ADCResult < LINE_SENSOR_MAX)
+	if(ADCResult < FORCE_SENSOR_MAX)
 		*operation = FINISHED_OPERATION;
 	else
 		*operation = CONTINUE_OPERATION;
 }
 
-void state_strength(int *operation)
+void state_strength(unsigned char *operation)
 {
-	__delay_cycles(50000);
+	delayMillis(50);
 	SetupADC(BIT3);
 	TakeADCMeas();
-	if(ADCResult < LINE_SENSOR_MAX)
+	if(ADCResult < FORCE_SENSOR_MAX)
 		*operation = FINISHED_OPERATION;
 	else
 		*operation = CONTINUE_OPERATION;
 }
 
-void state_finish(int *operation)
+void state_finish(unsigned char *operation)
 {
-	__delay_cycles(50000);
+	delayMillis(50);
 	SetupADC(BIT4);
 	TakeADCMeas();
 	if(ADCResult < FORCE_SENSOR_MAX)
@@ -262,16 +271,16 @@ __interrupt void ADC10_ISR(void)
 {
 	switch(__even_in_range(ADC10IV,ADC10IV_ADC10IFG))
 	{
-		case ADC10IV_NONE: break;               // No interrupt
-		case ADC10IV_ADC10OVIFG: break;         // conversion result overflow
-		case ADC10IV_ADC10TOVIFG: break;        // conversion time overflow
-		case ADC10IV_ADC10HIIFG: break;         // ADC10HI
-		case ADC10IV_ADC10LOIFG: break;         // ADC10LO
-		case ADC10IV_ADC10INIFG: break;         // ADC10IN
+		case ADC10IV_NONE: break;				// No interrupt
+		case ADC10IV_ADC10OVIFG: break;			// conversion result overflow
+		case ADC10IV_ADC10TOVIFG: break;		// conversion time overflow
+		case ADC10IV_ADC10HIIFG: break;			// ADC10HI
+		case ADC10IV_ADC10LOIFG: break;			// ADC10LO
+		case ADC10IV_ADC10INIFG: break;			// ADC10IN
 		case ADC10IV_ADC10IFG: 
 			ADCResult = ADC10MEM0;
 			__bic_SR_register_on_exit(CPUOFF);                                              
-			break;                          // Clear CPUOFF bit from 0(SR)                         
+			break;								// Clear CPUOFF bit from 0(SR)                         
 		default: break;
 	}
 }

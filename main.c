5dbd15f5b149709342c4c7a84371c80e6f16f6b3
Ryan <rtripod@gmail.com>
2013-10-10 22:38:23 +0800

    Added pwm control

--- main.c
+++ main.c
@@ -6,17 +6,43 @@
 
 #define STATE_DIR P3DIR
 #define STATE_OUT P3OUT
-#define STATE_LED0 BIT4
-#define STATE_LED1 BIT5
-#define STATE_LED2 BIT6
-#define STATE_LED3 BIT7
 
 #define GEARED_DIR P2DIR
 #define GEARED_OUT P2OUT
 #define GEARED_PIN BIT0
 
+#define DUCK1_SERVO BIT0
+#define DUCK2_SERVO BIT2
+#define DUCK3_SERVO BIT4
+#define STRENGTH_SERVO BIT6
+#define LIFT_MOTOR BIT1
+
+#define ENTRY_FSR BIT1
+#define PRE_DUCKS_FSR BIT3
+#define EXIT_FSR BIT5
+
+#define DUCK_IR BIT3
+#define STRENGTH_IR BIT4
+
+#define FSR_PORT 1
+#define IR_PORT 3
+
+#define SERVO_DIR P1DIR
+#define SERVO_SEL0 P1SEL0
+
+#define LIFT_DIR P2DIR
+#define LIFT_SEL0 P2SEL0
+
+#define FSR_SEL0 P1SEL0
+#define FSR_SEL1 P1SEL1
+
+#define IR_SEL0 P3SEL0
+#define IR_SEL1 P3SEL1
+
 #define FINISHED_OPERATION 0
 #define CONTINUE_OPERATION 1
+
+#define DUCK_DELAY 100
 
 typedef enum {IDLE = 0x00, FERRIS = 0x10, PRE_DUCKS = 0x20, DUCK1 = 0x30, DUCK2 = 0x40, DUCK3 = 0x50, STRENGTH = 0x60} STATE;
 
@@ -39,11 +65,61 @@ void delayMillis(unsigned int delay)
 	}
 }
 
-void SetupADC(unsigned int in_bit)
+// duty_cycle range: 700 - 2300
+void pwmControl(unsigned char in_component, int duty_cycle)
+{
+	switch (in_component)
+	{
+		case DUCK1_SERVO:
+			TA0CCR0 = 20000 - 1;						// PWM Period
+			TA0CCTL1 = OUTMOD_7;						// CCR1 reset/set
+			TA0CCR1 = duty_cycle;						// CCR1 PWM duty cycle
+			TA0CTL = TASSEL_2 + MC_1 + TACLR;			// SMCLK, up mode, clear TAR
+			break;
+		case DUCK2_SERVO:
+			TA1CCR0 = 20000 - 1;						// PWM Period
+			TA1CCTL1 = OUTMOD_7;						// CCR1 reset/set
+			TA1CCR1 = duty_cycle;						// CCR1 PWM duty cycle
+			TA1CTL = TASSEL_2 + MC_1 + TACLR;			// SMCLK, up mode, clear TAR
+			break;
+		case DUCK3_SERVO:
+			TB0CCR0 = 20000 - 1;						// PWM Period
+			TB0CCTL1 = OUTMOD_7;						// CCR1 reset/set
+			TB0CCR1 = duty_cycle;						// CCR1 PWM duty cycle
+			TB0CTL = TASSEL_2 + MC_1 + TACLR;			// SMCLK, up mode, clear TAR
+			break;
+		case STRENGTH_SERVO:
+			TB1CCR0 = 20000 - 1;						// PWM Period
+			TB1CCTL1 = OUTMOD_7;						// CCR1 reset/set
+			TB1CCR1 = duty_cycle;						// CCR1 PWM duty cycle
+			TB1CTL = TASSEL_2 + MC_1 + TACLR;			// SMCLK, up mode, clear TAR
+			break;
+		case LIFT_MOTOR:
+			TB2CCR0 = 20000 - 1;						// PWM Period
+			TB2CCTL1 = OUTMOD_7;						// CCR1 reset/set
+			TB2CCR1 = duty_cycle;						// CCR1 PWM duty cycle
+			TB2CTL = TASSEL_2 + MC_1 + TACLR;			// SMCLK, up mode, clear TAR
+			break;
+		default: break;
+	}
+}
+
+void SetupADC(unsigned char in_port, unsigned char in_bit)
 {
 	// Configure ADC
-	P1SEL1 |= in_bit;  
-	P1SEL0 |= in_bit; 
+	switch (in_port)
+	{
+		case FSR_PORT:
+			FSR_SEL0 |= in_bit;
+			FSR_SEL1 |= in_bit;
+			break;
+		case IR_PORT:
+			IR_SEL1 |= in_bit;
+			IR_SEL0 |= in_bit;
+			in_bit = in_bit << 6;			// Shift above BIT5 for below case statement (avoids nested control flow)
+			break;
+		default: break;
+	}
 
 	// Allow for settling delay 
 	delayMillis(50);
@@ -70,6 +146,15 @@ void SetupADC(unsigned int in_bit)
 			break;
 		case BIT4:
 			ADC10MCTL0 = ADC10INCH_4;
+			break;
+		case BIT5:
+			ADC10MCTL0 = ADC10INCH_5;
+			break;
+		case BIT6:
+			ADC10MCTL0 = ADC10INCH_12;
+			break;
+		case BIT7:
+			ADC10MCTL0 = ADC10INCH_13;
 			break;
 	}	
 	
@@ -109,7 +194,18 @@ int main(void)
 	// Initial state
 	STATE state = IDLE;
 	STATE_OUT = IDLE;
-	SetupADC(BIT0);
+	SetupADC(FSR_PORT, BIT1);
+	
+	// Setup servos
+	SERVO_DIR |= (DUCK1_SERVO+DUCK2_SERVO);
+	SERVO_SEL0 |= (DUCK1_SERVO+DUCK2_SERVO);
+	
+	pwmControl(DUCK1_SERVO, 1000);
+	pwmControl(DUCK2_SERVO, 1000);
+	
+	// Setup lift motor
+	LIFT_DIR |= LIFT_MOTOR;
+	LIFT_SEL0 |= LIFT_MOTOR;
 	
 	unsigned char operation;
 	while(1)
@@ -160,8 +256,8 @@ void state_machine(STATE *state, unsigned char operation)
 				case FINISHED_OPERATION:
 					*state = FERRIS;
 					STATE_OUT = FERRIS;
-					SetupADC(BIT1);
-					P2OUT |= BIT0;		// Turn on geared motor
+					SetupADC(FSR_PORT, PRE_DUCKS_FSR);
+					GEARED_OUT |= GEARED_PIN;		// Turn on geared motor
 					break;
 				default: break;
 			}
@@ -172,7 +268,7 @@ void state_machine(STATE *state, unsigned char operation)
 				case FINISHED_OPERATION:
 					*state = PRE_DUCKS;
 					STATE_OUT = PRE_DUCKS;
-					SetupADC(BIT2);
+					SetupADC(IR_PORT, DUCK_IR);
 					break;
 				default: break;
 			}
@@ -183,6 +279,7 @@ void state_machine(STATE *state, unsigned char operation)
 				case FINISHED_OPERATION:
 					*state = DUCK1;
 					STATE_OUT = DUCK1;
+					pwmControl(DUCK1_SERVO, 1500);
 					break;
 				default: break;
 			}
@@ -193,6 +290,7 @@ void state_machine(STATE *state, unsigned char operation)
 				case FINISHED_OPERATION:
 					*state = DUCK2;
 					STATE_OUT = DUCK2;
+					pwmControl(DUCK2_SERVO, 1500);
 					break;
 				default: break;
 			}
@@ -203,7 +301,7 @@ void state_machine(STATE *state, unsigned char operation)
 				case FINISHED_OPERATION:
 					*state = DUCK3;
 					STATE_OUT = DUCK3;
-					SetupADC(BIT3);
+					SetupADC(IR_PORT, STRENGTH_IR);
 					break;
 				default: break;
 			}
@@ -214,7 +312,7 @@ void state_machine(STATE *state, unsigned char operation)
 				case FINISHED_OPERATION:
 					*state = STRENGTH;
 					STATE_OUT = STRENGTH;
-					SetupADC(BIT4);
+					SetupADC(FSR_PORT, EXIT_FSR);
 					break;
 				default: break;
 			}
@@ -225,7 +323,9 @@ void state_machine(STATE *state, unsigned char operation)
 				case FINISHED_OPERATION:
 					*state = IDLE;
 					STATE_OUT = IDLE;
-					SetupADC(BIT0);
+					SetupADC(FSR_PORT, ENTRY_FSR);
+					pwmControl(DUCK1_SERVO, 1000);
+					pwmControl(DUCK2_SERVO, 1000);
 					GEARED_OUT &= ~GEARED_PIN;		// Turn off geared motor
 					break;
 				default: break;
@@ -267,13 +367,13 @@ void state_pre_ducks(unsigned char *operation)
 
 void state_duck1(unsigned char *operation)
 {
-	delayMillis(100);
+	delayMillis(DUCK_DELAY);
 	*operation = FINISHED_OPERATION;
 }
 
 void state_duck2(unsigned char *operation)
 {
-	delayMillis(100);
+	delayMillis(DUCK_DELAY);
 	*operation = FINISHED_OPERATION;
 }
 
@@ -309,8 +409,8 @@ __interrupt void ADC10_ISR(void)
 		case ADC10IV_ADC10INIFG: break;			// ADC10IN
 		case ADC10IV_ADC10IFG: 
 			ADCResult = ADC10MEM0;
-			__bic_SR_register_on_exit(CPUOFF);                                              
-			break;								// Clear CPUOFF bit from 0(SR)                         
+			__bic_SR_register_on_exit(CPUOFF);
+			break;								// Clear CPUOFF bit from 0(SR)
 		default: break;
 	}
 }
